1a6b9390f9e5d29a748626a167049f57312e1417
samandareo <smdeveloper04@gmail.com>
2025-04-17 14:59:39 +0500

    change server url

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import editIcon from "@/app/image/edit.svg";
 import Image from "next/image";
 
-const ServerURL = "http://localhost:4000/soap";
+const ServerURL = "https://soapcrud.onrender.com/soap";
 
 type typeUser = {
   id: string;
